9e804a3ccbadf6512395bd1872a487c47ac7843a
kenadams804-code <kenadams804@protonmail.com>
2026-06-20 22:02:24 +0800

    Create AppConfig.kt

---
+++ app/src/main/java/com/example/hfpdetector/AppConfig.kt
@@ -0,0 +1,13 @@
+package com.example.hfpdetector
+
+object AppConfig {
+    const val CONTROL_PORT = 39090
+    const val HELLO_INTERVAL_MS = 1500L
+
+    const val CH_PERSIST = "lancall_persist"
+    const val CH_CALL = "lancall_call"
+
+    const val NID_PERSIST = 1001
+    const val NID_INCOMING = 2001
+    const val NID_ONGOING = 2002
+}
